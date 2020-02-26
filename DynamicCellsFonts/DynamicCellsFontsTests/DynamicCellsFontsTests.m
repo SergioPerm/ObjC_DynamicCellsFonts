73baa0697c518d21074f660f2c482baebc855f9b
SergioPerm <itotdel@list.ru>
2020-02-26 20:21:02 +0500

    first commit

---
+++ DynamicCellsFonts/DynamicCellsFontsTests/DynamicCellsFontsTests.m
@@ -0,0 +1,37 @@
+//
+//  DynamicCellsFontsTests.m
+//  DynamicCellsFontsTests
+//
+//  Created by kluv on 26/02/2020.
+//  Copyright © 2020 com.kluv.hw24. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DynamicCellsFontsTests : XCTestCase
+
+@end
+
+@implementation DynamicCellsFontsTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
