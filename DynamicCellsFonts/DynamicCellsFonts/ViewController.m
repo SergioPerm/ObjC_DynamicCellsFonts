5966822bc1cddd2ef3ab990252fbb3857c4c1174
SergioPerm <itotdel@list.ru>
2020-03-04 20:56:02 +0500

    - output all fonts in dynamic cells with family names sections.

--- DynamicCellsFonts/DynamicCellsFonts/ViewController.m
+++ DynamicCellsFonts/DynamicCellsFonts/ViewController.m
@@ -19,5 +19,55 @@
     // Do any additional setup after loading the view.
 }
 
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    
+    return [[UIFont familyNames] count];
+    
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    NSArray* familyNames = [UIFont familyNames];
+    NSArray* fontNames = [UIFont fontNamesForFamilyName:[familyNames objectAtIndex:section]];
+    
+    return [fontNames count];
+    
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    
+    NSArray* familyNames = [UIFont familyNames];
+   
+    NSString* familyName = [familyNames objectAtIndex:section];
+    
+    return familyName;
+    
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString* identifier = @"Cell";
+    
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+        
+        NSLog(@"Created cell!");
+    }
+    
+    NSArray* familyNames = [UIFont familyNames];
+    NSArray* fontNames = [UIFont fontNamesForFamilyName:[familyNames objectAtIndex:indexPath.section]];
+    
+    NSString* fontName = [fontNames objectAtIndex:indexPath.row];
+    
+    UIFont* font = [UIFont fontWithName:fontName size:16];
+    
+    cell.textLabel.font = font;
+    cell.textLabel.text = fontName;
+    
+    return cell;
+    
+}
 
 @end
